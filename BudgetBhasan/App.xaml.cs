2a77258d9fc1974aa9c54a786cd61c7fef9898f4
RujenMaharjan <rujenmazn@gmail.com>
2024-12-16 15:37:17 +0545

    my first commit

---
+++ BudgetBhasan/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetBhasan;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ BudgetBhasan/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BudgetBhasan;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
